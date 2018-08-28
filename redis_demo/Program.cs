6d5be61a28e8c28856aaef8744b22b7fe9b68051
Charles DeSouza <charles5388@gmail.com>
2018-08-28 15:27:54 +0530

    Initial Commit

---
+++ redis_demo/Program.cs
@@ -0,0 +1,19 @@
+﻿using StackExchange.Redis;
+using System;
+
+namespace redis_demo
+{
+  class Program
+  {
+    public static void Main(string[] args)
+    {
+      using(ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("192.168.99.100:6379"))
+      {
+        IDatabase db = redis.GetDatabase();
+        string value = db.StringGet("name");
+        Console.WriteLine(value); 
+      }
+
+    }
+  }
+}
